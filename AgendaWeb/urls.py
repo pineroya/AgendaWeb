bf9ca55c6e3484c6eac2bcf3156cfee61b594a48
Yamil <69180694+pineroya@users.noreply.github.com>
2023-07-05 10:23:24 -0300

    Update urls.py agendaweb

--- AgendaWeb/urls.py
+++ AgendaWeb/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,3 +28,4 @@ urlpatterns = [
     url('', include('homeApp.urls')),
     url('', include('notasApp.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
