af03486a561cb5c31a88ef98096a34f24ae3b1a7
pineroya <yamil.rokr@gmail.com>
2023-07-06 12:39:12 -0300

    configuracion postgreSQL

--- AgendaWeb/settings.py
+++ AgendaWeb/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'AgendaWeb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dbagendaweb',
+        'USER': 'postgres',
+        'PASSWORD': 'wixo2020',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
