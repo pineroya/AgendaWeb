22bc71454f62bd5bd5d312667d36c8ee0179ceb4
pineroya <yamil.rokr@gmail.com>
2023-07-06 11:21:29 -0300

    sin database sqlite3

--- AgendaWeb/urls.py
+++ AgendaWeb/urls.py
@@ -29,3 +29,4 @@ urlpatterns = [
     url('', include('notasApp.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
